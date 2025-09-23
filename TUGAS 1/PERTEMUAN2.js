0b5bf77a5f1405a42b1291e6d8b91883ba2684f8
RANGGA518 <ranggaardiansyah518@gmail.com>
2025-09-24 02:24:39 +0700

    Add files via upload

---
+++ TUGAS 1/PERTEMUAN2.js
@@ -0,0 +1,49 @@
+// // VARIABEL JAVASCRIPT
+// var nama = "RANGGA PUTRA ARDIANSYAH";
+// var umur = 21;
+// var alamat = "Kradenan";
+// let status = "Mahasiswa Aktif"
+
+
+// // MENAMPILKAN ISI VARIABEL
+// document.write("Nama : " + nama + "<br>");
+// document.write("Usia sekarang : " + umur + "<br>");
+// document.write("Alamat : " + alamat + "<br>");
+// document.write("Status : " + status + "<br>");
+
+// // MENGISI ULANG VARIABEL
+// var umur = 24;
+// umur = 15;
+// document.write("Usia 6 tahun yang lalu : " + umur + "<br>");
+
+// // MENGHAPUS VARIABEL
+// Rumah = "Kradenan";
+// delete Rumah;
+// Jalan = "Grajagan"
+// document.write("Alamat Rumah :" + Rumah + "<br>");
+// document.write("Nama Jalan : " + Jalan + "<br>");
+
+// DIALOG ALERT
+// window.alert("Hello world!");
+// alert("Hello Kawan");
+
+// DIALOG PROMPT
+// var jeneng = prompt("Sopo Jenengmu? :", "");
+// document.write("<p>Jenengku " + jeneng + "</p>");
+
+// DIALOG PROMPT EKSTERNAL
+// var pw = prompt("Masukkan Password : ");
+
+// if(pw=="Rangga") {
+//     document.write("<p>Password Anda Benar " + pw + "</p>")
+// } else {
+//     alert("Maaf, Password Anda salah")
+//     window.location= "PERTEMUAN2.html"
+// }
+
+// DIALOG CONFIRM
+
+
+
+
+
